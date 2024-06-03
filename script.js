1c2e26b279a72b485f3bf264dba00e91982170ad
Anderson Gabriel <gabrielvalencasa98@gmail.com>
2024-06-03 17:53:28 -0300

    Projeto Finalizado

--- script.js
+++ script.js
@@ -1,3 +1,23 @@
 function googleTranslateElementInit() {
-  new google.translate.TranslateElement({pageLanguage: 'pt-BR'}, 'google_translate_element');
+  new google.translate.TranslateElement({pageLanguage: 'en', layout: google.translate.TranslateElement.InlineLayout.SIMPLE}, 'google_translate_element');
 }
+
+document.addEventListener('DOMContentLoaded', function() {
+
+  var forms = document.getElementsByClassName('needs-validation');
+
+  var validation = Array.prototype.filter.call(forms, function(form) { // Foi uma forma mais simples de fazer a validação do que for each, por eu estar usando Bootstrap
+      form.addEventListener('submit', function(event) {
+          if (form.checkValidity() === false) {
+              event.preventDefault();
+              event.stopPropagation();
+          } else {
+              event.preventDefault();
+              alert('Enviado com sucesso!');
+          }
+          form.classList.add('was-validated');
+      }, false);
+  });
+});
+
+
